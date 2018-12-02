0dd260fb5c0c51bec405eface0456fc02ec5a3aa
jinseongjoo <qpyou1234@naver.com>
2018-12-02 14:43:03 +0900

    2.3 Modifying the DOM with JS

--- Hello World/index.js
+++ Hello World/index.js
@@ -1,3 +1,4 @@
-const title = document.getElementById("title")
-
+const title = document.querySelector("#title")
 title.innerHTML = "Hi! From JS"
+title.style.color = "red"
+document.title = "I own you now"
